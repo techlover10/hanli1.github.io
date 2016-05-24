0d177ed659362f2da90d93014fc94bd3cedaa5f3
Han Li <hli@p1analytics.com>
2016-05-23 21:34:53 -0400

    polished transition animations

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 fadeWindowValue = 500; 
 
 $( document ).ready(function() {
-  $(".button-collapse").sideNav();
-  loadSideBarDynamically();
-  $('#about')[0].click();
-  //$('#projects')[0].click();
+	$(".button-collapse").sideNav();
+	loadSideBarDynamically();
+	$('#about')[0].click();
+	//$('#projects')[0].click();
 });
 
 //makes all elements including picture on main page load at once
@@ -13,66 +13,74 @@ $( document ).ready(function() {
 // });
 
 $(window).on('load', function() {
-    $("#cover").fadeOut(1000);
+	$("#cover").fadeOut(1000);
 });
 
 
 // tab onclick listeners
 $('ul#tabs li').click(function() 
-  { 
-    removeActives();
-    setAsActive('#'+this.id);
-    window.scrollTo(0, 0);
-  });
+{ 
+	removeActives();
+	setAsActive('#'+this.id);
+	window.scrollTo(0, 0);
+});
 
 /*
-  Handle sidebar clicks, instead of individual ones, it simply gets the id of the tab button
-  from the sidebar item class, then sends a click to the button
-  */
-$('#sidebar').on('click','li',function(e){
-  id = $(this).attr('id');
-  $('#'+ id.substr(0, id.length - 8)).click();
-  e.preventDefault();
-  $('.button-collapse').sideNav('hide');
-});
+	Handle sidebar clicks, instead of individual ones, it simply gets the id of the tab button
+	from the sidebar item class, then sends a click to the button
+	*/
+	$('#sidebar').on('click','li',function(e){
+		id = $(this).attr('id');
+		$('#'+ id.substr(0, id.length - 8)).click();
+		e.preventDefault();
+		$('.button-collapse').sideNav('hide');
+	});
 /*generates the sidebar based on the elements in the top tabs
 removes the wave effect as sidebar has it built in
 adds a class with the id in order to keep highlighting consistant
 */
 function loadSideBarDynamically()
 {
-  $('#tabs').children('li').each(function () {
-    sidebarItem = $(this).clone().removeClass("waves-effect").attr('id', $(this).attr('id') + '_sidebar');
-    $('#sidebar').append(sidebarItem);
-  });
+	$('#tabs').children('li').each(function () {
+		sidebarItem = $(this).clone().removeClass("waves-effect").attr('id', $(this).attr('id') + '_sidebar');
+		$('#sidebar').append(sidebarItem);
+	});
 
 }
 
 function removeActives()
 {
-  $('#tabs').children('li').each(function () {
-    $(this).removeClass("active");
-  });
-  $('#sidebar').children('li').each(function () {
-    $(this).removeClass("active");
-  });
+	$('#tabs').children('li').each(function () {
+		$(this).removeClass("active");
+	});
+	$('#sidebar').children('li').each(function () {
+		$(this).removeClass("active");
+	});
 }
 
 function setAsActive(id)
 {
-  $(id).addClass("active");
-  $(id + '_sidebar').addClass("active");
-  //strip the # fom the id
-  var fileName = id.substr(1, id.length-1) + '.html';
-  if(fileName === 'projects.html')
-  {
-    $('#main_container').hide().load(fileName, function() {
-      $('#doorlock')[0].click();
-    }).fadeIn(fadeWindowValue);
-  }  
-  else
-  {
-    $('#main_container').hide().load(fileName).fadeIn(fadeWindowValue);
-  }
-    
+	$(id).addClass("active");
+	$(id + '_sidebar').addClass("active");
+	//strip the # fom the id
+	var fileName = id.substr(1, id.length-1) + '.html';
+
+	//keep main container height before executing
+	$("#main_container").css("height",$("#main_container").height);
+
+	$('#main_container').fadeOut('fast', function(){
+		//when done fading out, load new html
+		$('#main_container').load(fileName, function(){
+			//when done with loading, fade it in
+			$('#main_container').fadeIn('main_container');
+			//let the size readjust
+			$("#main_container").css("height","auto");
+			//if it is the project tab, click first to load it
+			if(fileName === 'projects.html')
+			{
+				$('#doorlock')[0].click();
+			}
+		});
+	});
+
 }
